64915b6cd005b9a0f45a501a2368989929cdd44c
iglée42 <igleev2@gmail.com>
2022-02-04 15:39:12 +0100

    Adding wood and cobblestone recipes

---
+++ src/main/java/fr/iglee42/techresourcesbase/utils/CustomArmorMaterial.java
@@ -0,0 +1,83 @@
+package fr.iglee42.techresourcesbase.utils;
+
+import fr.iglee42.techresourcesbase.TechResourcesBase;
+import fr.iglee42.techresourcesbase.init.ModItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
+
+public enum CustomArmorMaterial implements IArmorMaterial {
+
+    ULTIMERITE(TechResourcesBase.MODID + ":ultimerite",770,new int[] {20,28,26,18},11, SoundEvents.ARMOR_EQUIP_NETHERITE,20.0f,5.0f,()->{
+        return Ingredient.of(ModItem.ULTIMERITE_INGOT.get());
+    });
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    CustomArmorMaterial(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability, SoundEvent soundEvent, float toughness, float knockbackResistance, Supplier<Ingredient> repairMaterial) {
+        this.name = name;
+        this.maxDamageFactor = maxDamageFactor;
+        this.damageReductionAmountArray = damageReductionAmountArray;
+        this.enchantability = enchantability;
+        this.soundEvent = soundEvent;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairMaterial = new LazyValue<>(repairMaterial);
+    }
+
+    @Override
+    public int getDurabilityForSlot(EquipmentSlotType slotIn) {
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+    }
+
+    @Override
+    public int getDefenseForSlot(EquipmentSlotType slotIn) {
+        return this.damageReductionAmountArray[slotIn.getIndex()];
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.soundEvent;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairMaterial.get();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public String getName() {
+        return this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    /**
+     * Gets the percentage of knockback resistance provided by armor of the material.
+     */
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
