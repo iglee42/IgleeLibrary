20f576ae24c57149e5bc9e91daadaec3e2ce28f4
iglée42 <igleev2@gmail.com>
2022-11-06 17:34:16 +0100

    Update 1.4.5

--- src/main/java/fr/iglee42/techresourcesbase/utils/InventoryUtil.java
+++ src/main/java/fr/iglee42/techresourcesbase/utils/InventoryUtil.java
@@ -1,8 +1,13 @@
 package fr.iglee42.techresourcesbase.utils;
 
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.level.block.Block;
 
 public class InventoryUtil {
     public static boolean hasPlayerStackInInventory(Player player, Item item) {
@@ -25,5 +30,23 @@ public class InventoryUtil {
         }
 
         return -1;
+    }
+
+    public static Item getItem(String name){
+        String[] it = ModsUtils.split(name,":");
+        return Registry.ITEM.get(new ResourceLocation(it[0],it[1]));
+    }
+
+    public static Block getBlock(String name){
+        String[] it = ModsUtils.split(name,":");
+        return Registry.BLOCK.get(new ResourceLocation(it[0],it[1]));
+    }
+    public static Enchantment getEnchantment(String name){
+        String[] it = ModsUtils.split(name,":");
+        return Registry.ENCHANTMENT.get(new ResourceLocation(it[0],it[1]));
+    }
+    public static EntityType<?> getEntity(String name){
+        String[] it = ModsUtils.split(name,":");
+        return Registry.ENTITY_TYPE.get(new ResourceLocation(it[0],it[1]));
     }
 }
--- src/main/java/fr/iglee42/techresourcesbase/utils/JsonHelper.java
+++ src/main/java/fr/iglee42/techresourcesbase/utils/JsonHelper.java
@@ -16,6 +16,9 @@ public class JsonHelper {
     public static String getString(JsonObject json, String name){
         return json.get(name).getAsString();
     }
+    public static int getInt(JsonObject json, String name){
+        return json.get(name).getAsInt();
+    }
     public static Item getItem(JsonObject json, String name){
         String[] it = ModsUtils.split(getString(json,name),":");
         return Registry.ITEM.get(new ResourceLocation(it[0],it[1]));
@@ -34,6 +37,40 @@ public class JsonHelper {
         String[] it = ModsUtils.split(getString(json,name),":");
         return Registry.ENCHANTMENT.get(new ResourceLocation(it[0],it[1]));
     }
+    public static boolean getBooleanOrDefault(JsonObject json, String name,boolean def){
+        if (!json.has(name)) return def;
+        return json.get(name).getAsBoolean();
+    }
+    public static String getStringOrDefault(JsonObject json, String name,String def){
+        if (!json.has(name)) return def;
+        return json.get(name).getAsString();
+    }
 
+    public static int getIntOrDefault(JsonObject json,String name,int def){
+        if (!json.has(name)) return def;
+        return json.get(name).getAsInt();
+    }
+    public static Item getItemOrDefault(JsonObject json, String name,Item def){
+        if (!json.has(name)) return def;
+        String[] it = ModsUtils.split(getString(json,name),":");
+        return Registry.ITEM.get(new ResourceLocation(it[0],it[1]));
+    }
+
+    public static Block getBlockOrDefault(JsonObject json, String name,Block def){
+        if (!json.has(name)) return def;
+        String[] it = ModsUtils.split(getString(json, name),":");
+        return Registry.BLOCK.get(new ResourceLocation(it[0],it[1]));
+    }
+
+    public static EntityType<?> getEntityTypeOrDefault(JsonObject json, String name,EntityType<?> def){
+        if (!json.has(name)) return def;
+        String[] it = ModsUtils.split(getString(json,name),":");
+        return Registry.ENTITY_TYPE.get(new ResourceLocation(it[0],it[1]));
+    }
+    public static Enchantment getEnchantmentOrDefault(JsonObject json, String name,Enchantment def){
+        if (!json.has(name)) return def;
+        String[] it = ModsUtils.split(getString(json,name),":");
+        return Registry.ENCHANTMENT.get(new ResourceLocation(it[0],it[1]));
+    }
 
 }
