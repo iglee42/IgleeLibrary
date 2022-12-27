2d2962c9805b6bdd5521a71d2eda1a0b4cedf10d
iglée42 <igleev2@gmail.com>
2022-12-27 15:46:48 +0100

    Reorganize Structure of Mod Adding SecondBlockEntity Removing Utimerite Armor Craft

---
+++ src/main/java/fr/iglee42/techresourcesbase/api/blockentities/SecondBlockEntity.java
@@ -0,0 +1,39 @@
+package fr.iglee42.techresourcesbase.api.blockentities;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+public abstract class SecondBlockEntity extends BlockEntity {
+
+    private int tick;
+
+    public SecondBlockEntity(BlockEntityType<?> p_155228_, BlockPos p_155229_, BlockState p_155230_) {
+        super(p_155228_, p_155229_, p_155230_);
+    }
+
+    public void tick(Level level, BlockPos pos, BlockState state,SecondBlockEntity entity){
+        entity.tick++;
+        if (entity.tick == 20) {
+            entity.tick = 0;
+            entity.second(level, pos, state, entity);
+        }
+    }
+
+    protected abstract void second(Level level, BlockPos pos, BlockState state,SecondBlockEntity entity);
+
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        tag.putInt("tick",tick);
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        tick = tag.getInt("tick");
+    }
+}
