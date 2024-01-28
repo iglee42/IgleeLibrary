f0372eba2c955516ff9704e45827290de3869644
iglée42 <igleev2@gmail.com>
2024-01-28 11:50:08 +0100

    Fixes

--- src/main/java/fr/iglee42/igleelib/api/utils/ITickableRecipe.java
+++ src/main/java/fr/iglee42/igleelib/api/utils/ITickableRecipe.java
@@ -1,20 +1,20 @@
 package fr.iglee42.igleelib.api.utils;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.block.BlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public interface ITickableRecipe<B extends BlockEntity> {
+public interface ITickableRecipe<B extends TileEntity> {
 
-    default void start(Level level, BlockPos pos, BlockState state, B be){}
+    default void start(World level, BlockPos pos, BlockState state, B be){}
 
-    default void tick(Level level, BlockPos pos, BlockState state,int progress, B be){}
+    default void tick(World level, BlockPos pos, BlockState state,int progress, B be){}
 
-    default void second(Level level, BlockPos pos, BlockState state,int progress, B be){}
+    default void second(World level, BlockPos pos, BlockState state, int progress, B be){}
 
-    default void finish(Level level, BlockPos pos, BlockState state, B be){}
+    default void finish(World level, BlockPos pos, BlockState state, B be){}
 
-    boolean canContinue(Level level, BlockPos pos, BlockState state,int progress, B be);
+    boolean canContinue(World level, BlockPos pos, BlockState state, int progress, B be);
 
 }
