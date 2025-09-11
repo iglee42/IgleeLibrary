d0d3521a0f5a0af95cf72a61f2fff5bf06f02fbc
iglée42 <igleev2@gmail.com>
2025-09-11 21:00:01 +0200

    Added a class parser for custom classes parsing

--- src/main/java/fr/iglee42/igleelib/IgleeLibrary.java
+++ src/main/java/fr/iglee42/igleelib/IgleeLibrary.java
@@ -1,5 +1,6 @@
 package fr.iglee42.igleelib;
 
+import com.google.gson.JsonElement;
 import fr.iglee42.igleelib.common.init.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
@@ -13,11 +14,17 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
+
 
 @Mod(IgleeLibrary.MODID)
 public class IgleeLibrary {
 
     public static final String MODID = "igleelib";
+    private static final HashMap<Class<?>,Function<? extends JsonElement,Object>> CLASS_PARSERS = new HashMap<>();;
 
     public IgleeLibrary() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -41,6 +48,16 @@ public class IgleeLibrary {
             ForgeRegistries.ITEMS.getKeys().stream().filter(rs-> rs.getNamespace().equals(MODID)).forEach(rs->
                     event.accept(ForgeRegistries.ITEMS.getValue(rs)));
         }
+    }
+
+    public static <T, J extends JsonElement> void addClassParser(Class<T> clazz, Function<J,T> parser){
+        CLASS_PARSERS.put(clazz, (Function<? extends JsonElement, Object>) parser);
+    }
+
+    public static <T,J extends JsonElement> Function<J,T> getClassParser(Class<T> clazz){
+        if (CLASS_PARSERS.containsKey(clazz))
+            return (Function<J, T>) CLASS_PARSERS.get(clazz);
+        throw new IllegalArgumentException("There isn't a class parse for " + clazz.getName());
     }
 
 }
